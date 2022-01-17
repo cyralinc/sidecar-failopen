78106425a9f921688c4397e4b75b41e14fa4af83
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2022-01-17 15:53:31 -0300

    Adds denodo repository to golang code

---
+++ internal/repository/denodo/denodo.go
@@ -0,0 +1,64 @@
+package denodo
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cyralinc/sidecar-failopen/internal/config"
+	"github.com/cyralinc/sidecar-failopen/internal/keys"
+	"github.com/cyralinc/sidecar-failopen/internal/logging"
+	"github.com/cyralinc/sidecar-failopen/internal/repository"
+	"github.com/cyralinc/sidecar-failopen/internal/repository/genericsql"
+
+	// Postgresql DB driver
+	_ "github.com/lib/pq"
+)
+
+// PostgreSQL is the name registered by the DB driver.
+const PostgreSQL = "postgres"
+
+type denodoRepository struct {
+	// The majority of the repository.Repository functionality is delegated to
+	// a generic SQL repository instance (genericSqlRepo).
+	genericSqlRepo *genericsql.GenericSqlRepository
+}
+
+// *postgresqlRepository implements repository.Repository
+var _ repository.Repository = (*denodoRepository)(nil)
+
+func NewDenodoRepository(_ context.Context, cfg config.RepoConfig) (repository.Repository, error) {
+
+	connStr := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%d/%s",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.Database,
+	)
+
+	logging.Info("instantiating postgres repository at %s:%d", cfg.Host, cfg.Port)
+
+	sqlRepo, err := genericsql.NewGenericSqlRepository(cfg.RepoName, PostgreSQL, cfg.Database, connStr)
+	if err != nil {
+		return nil, fmt.Errorf("could not instantiate generic sql repository: %w", err)
+	}
+
+	return &denodoRepository{genericSqlRepo: sqlRepo}, nil
+}
+
+func (repo *denodoRepository) Ping(ctx context.Context) error {
+	return repo.genericSqlRepo.Ping(ctx)
+}
+
+func (repo *denodoRepository) Close() error {
+	return repo.genericSqlRepo.Close()
+}
+
+func (repo *denodoRepository) Type() string {
+	return keys.DenodoRepoKey
+}
+
+func init() {
+	repository.Register(keys.DenodoRepoKey, NewDenodoRepository)
+}
